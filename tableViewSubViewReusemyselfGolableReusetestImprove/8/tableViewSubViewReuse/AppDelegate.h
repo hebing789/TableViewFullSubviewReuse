a026010c7749001f7288f92185900e196871dd1f
bin_he <bin_he@intsig.net>
2016-11-24 15:45:59 +0800

    奔溃bug原因注释和修改

---
+++ tableViewSubViewReusemyselfGolableReusetestImprove/8/tableViewSubViewReuse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewSubViewReuse
+//
+//  Created by 何兵 on 11/17/16.
+//  Copyright © 2016 何兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
